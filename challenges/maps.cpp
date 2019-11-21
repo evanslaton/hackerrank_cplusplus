f99ded03548020c45286b065de34868765f2a11a
Evan Slaton <evanslaton@gmail.com>
2019-11-21 13:54:40 -0800

    Finished maps challenge

---
+++ challenges/maps.cpp
@@ -0,0 +1,38 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+	int length;
+	cin >> length;
+	map<string, int>m;
+	int queryType, score;
+	string name;
+	for (int i = 0; i < length; i++) {
+		cin >> queryType >> name;
+		if (queryType == 1) {
+			cin >> score;
+			map<string, int>::iterator itr = m.find(name);
+			if (itr != m.end()) {
+				score += m[name];
+				m.find(name)->second = score;
+			}
+			else {
+				m.insert(make_pair(name, score));
+			}
+		}
+		else if (queryType == 2) {
+			m.erase(name);
+		}
+		else if (queryType == 3) {
+			cout << m[name] << endl;
+		}
+	}
+	return 0;
+}
